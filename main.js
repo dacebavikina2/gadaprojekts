6196bf67a126d3ff18bb7640b52fa545db0b9182
dacebavikina2 <dacebavikina2@gmail.com>
2023-05-12 12:28:26 +0300

    json un js izstrāde

---
+++ main.js
@@ -0,0 +1,19 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+  
+    // Make a request to the JSON file using AJAX
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'users.json', true);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        var users = JSON.parse(xhr.responseText);
+        if (username in users && users[username] == password) {
+          alert("Login successful!");
+        } else {
+          alert("Invalid username or password.");
+        }
+      }
+    };
+    xhr.send();
+  }
